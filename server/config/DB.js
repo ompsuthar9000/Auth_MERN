531bf7e74dd26cb665edce44a9dc5631bc34bec3
Om Suthar <ompsuthar9000@gmail.com>
2024-12-30 20:41:57 +0530

    varcel.json updated

--- server/config/DB.js
+++ server/config/DB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-const dburl = process.env.DBURl.replace('<db_password>', process.env.DBPASSWORD)
+const dburl = process.env.DBURL.replace('<db_password>', process.env.DBPASSWORD)
 
 const connectDb = async () => {
     try {
